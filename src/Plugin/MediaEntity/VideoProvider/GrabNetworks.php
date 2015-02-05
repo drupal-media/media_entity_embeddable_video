85f89e2667cffc9435b597d5a763b6a9cd82445a
Janez Urevc <janez@janezurevc.name>
2015-02-05 13:20:42 +0100

    by slashrsm: Implement Grab provider.

--- src/Plugin/MediaEntity/VideoProvider/GrabNetworks.php
+++ src/Plugin/MediaEntity/VideoProvider/GrabNetworks.php
@@ -6,8 +6,12 @@
 
 namespace Drupal\media_entity_embeddable_video\Plugin\MediaEntity\VideoProvider;
 
+use Drupal\Core\Config\Config;
+use Drupal\Core\Http\Client;
+use Drupal\Core\Url;
 use Drupal\media_entity_embeddable_video\VideoProviderBase;
 use Drupal\media_entity_embeddable_video\VideoProviderInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides embedding support for Grab videos.
@@ -25,10 +29,49 @@ use Drupal\media_entity_embeddable_video\VideoProviderInterface;
 class GrabNetworks extends VideoProviderBase implements VideoProviderInterface {
 
   /**
+   * Config object.
+   *
+   * @var \Drupal\Core\Config\Config
+   */
+  protected $config;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, Client $http_client, Config $config) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $http_client);
+    $this->config = $config;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('http_client'),
+      $container->get('config.factory')->get('media_entity_embeddable_video.settings')
+    );
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function thumbnailURI() {
+    $response = $this->httpClient->get(
+      Url::fromUri(
+        'http://content.grabnetworks.com/v/' . $this->matches['id'],
+        ['query' => ['from' => $this->config->get('grab_from')]]
+      )
+    );
 
+    if ($response->getStatusCode() == 200 && ($data = $response->json())) {
+      return $data['video']['media']['preview']['url'];
+    }
+
+    return FALSE;
   }
 
 }
