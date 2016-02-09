68fe894c4bcdef4035e2f63c50546bbfcdd8d0cf
marcingy <marcingy@gmail.com>
2016-02-09 11:03:05 -0700

    Add new method for default thumbnail

--- src/Plugin/MediaEntity/Type/EmbeddableVideo.php
+++ src/Plugin/MediaEntity/Type/EmbeddableVideo.php
@@ -239,12 +239,19 @@ class EmbeddableVideo extends MediaTypeBase implements EmbeddableVideoTypeInterf
   /**
    * {@inheritdoc}
    */
+  public function getDefaultThumbnail() {
+    return $this->config->get('icon_base') . '/embeddable_video.png';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function thumbnail(MediaInterface $media) {
     if ($local_image = $this->getField($media, 'image_local')) {
       return $local_image;
     }
 
-    return $this->config->get('icon_base') . '/embeddable_video.png';
+    return $this->getDefaultThumbnail();
   }
 
   /**
